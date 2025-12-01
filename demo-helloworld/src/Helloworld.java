d700872c271bb7af7356bc93c083fd3e418e323a
Keith <your_email@example.com>
2025-12-01 17:57:52 +0800

    da

---
+++ demo-helloworld/src/Helloworld.java
@@ -0,0 +1,13 @@
+public class Helloworld {
+  public static void main(String[] args) {
+    // Notes
+
+    // Tool
+    // 1. System.out.println("")
+
+    //Every lines of java line must end with ;
+    System.out.println("Hello world");
+    System.out.println("Hello world");
+    System.out.println("Hello world");
+  }
+}
